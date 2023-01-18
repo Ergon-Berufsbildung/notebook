fda3b088e10a61471022707285cbd4383751afd8
Niculin Steiner <niculin.steiner@ergon.ch>
2023-01-18 11:36:32 +0100

    Add Content class

---
+++ Java/JavaNotebookApp/src/main/java/ch/niculin/notebook/domain/model/Content.java
@@ -0,0 +1,17 @@
+package ch.niculin.notebook.domain.model;
+
+public class Content {
+    private String content;
+
+    public Content(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
