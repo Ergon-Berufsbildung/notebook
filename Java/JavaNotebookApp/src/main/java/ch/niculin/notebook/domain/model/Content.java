23a0c82fb76608168e8d033735850c693f12d17c
Niculin Steiner <niculin.steiner@ergon.ch>
2023-01-18 13:53:02 +0100

    Add repo with tests

--- Java/JavaNotebookApp/src/main/java/ch/niculin/notebook/domain/model/Content.java
+++ Java/JavaNotebookApp/src/main/java/ch/niculin/notebook/domain/model/Content.java
@@ -1,5 +1,7 @@
 package ch.niculin.notebook.domain.model;
 
+import java.util.Objects;
+
 public class Content {
     private String content;
 
@@ -13,5 +15,20 @@ public class Content {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Content content1 = (Content) o;
+
+        return Objects.equals(content, content1.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return content != null ? content.hashCode() : 0;
     }
 }
