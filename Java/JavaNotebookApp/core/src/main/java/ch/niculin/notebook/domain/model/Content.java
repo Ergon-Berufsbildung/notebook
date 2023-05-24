ebcf22928c7361e54c605449e4d43398f8f87bbd
Niculin Steiner <niculin.steiner@ergon.ch>
2023-05-24 08:16:54 +0200

    refactor unused imports

--- Java/JavaNotebookApp/core/src/main/java/ch/niculin/notebook/domain/model/Content.java
+++ Java/JavaNotebookApp/core/src/main/java/ch/niculin/notebook/domain/model/Content.java
@@ -1,10 +1,7 @@
 package ch.niculin.notebook.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonRootName;
 import com.fasterxml.jackson.annotation.JsonValue;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 public record Content(String content) {
     @Override
